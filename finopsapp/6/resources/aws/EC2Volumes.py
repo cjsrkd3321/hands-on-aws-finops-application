2a9fa6688434a4028b7828faf960800759d4d41b
rex <81349047+cjsrkd3321@users.noreply.github.com>
2023-11-14 19:29:08 +0900

    세션(클라이언트) 주입

---
+++ finopsapp/6/resources/aws/EC2Volumes.py
@@ -0,0 +1,55 @@
+from mypy_boto3_ec2 import EC2Client
+from mypy_boto3_ec2.type_defs import VolumeTypeDef
+
+from resources.base import (
+    DeleteResultType,
+    ListResultType,
+    TagType,
+    ResourceBase,
+    LIST_FUNCS,
+)
+
+
+class EC2Volume(ResourceBase):
+    def __init__(self, svc: EC2Client, volume: VolumeTypeDef) -> None:
+        self.svc: EC2Client = svc
+        self.volume = volume
+
+    def delete(self) -> DeleteResultType:
+        try:
+            self.svc.delete_volume(VolumeId=self.volume["VolumeId"])
+        except Exception as e:
+            return e
+        return None
+
+    @property
+    def tags(self) -> list[TagType]:
+        return self.volume.get("Tags", [])
+
+    def __str__(self) -> str | None:
+        return self.volume.get("VolumeId")
+
+
+def list_ec2_volumes(sess: EC2Client) -> ListResultType:
+    svc: EC2Client = sess
+    iterator = svc.get_paginator("describe_volumes").paginate(
+        Filters=[
+            {
+                "Name": "status",
+                "Values": ["available"],
+            }
+        ]
+    )
+
+    return (
+        EC2Volume(
+            svc,
+            volume,
+        )
+        for volumes in iterator
+        for volume in volumes["Volumes"]
+    )
+
+
+if __name__ != "__main__":
+    LIST_FUNCS.append(("ec2", list_ec2_volumes))
