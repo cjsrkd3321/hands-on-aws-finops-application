d99dc39b9565e2289c973987573cd29e46b678a9
rex <81349047+cjsrkd3321@users.noreply.github.com>
2023-11-15 22:44:21 +0900

    Update 7. aws global service.py

--- 사전준비/7. aws global service.py
+++ 사전준비/7. aws global service.py
@@ -3,7 +3,16 @@ import boto3
 regions = ["ap-northeast-2", "us-east-1", "us-west-1"]
 services = ["ec2", "iam", "cloudfront", "lambda"]
 
+no_duplicated = []
+
 for service in services:
     for region in regions:
         svc = boto3.client(service_name=service, region_name=region)  # type: ignore
+        real_region = svc._client_config.region_name
+
+        list_value = f"{service}!{real_region}"
+        if list_value in no_duplicated:
+            continue
+
         print(service, region, svc._client_config.region_name)
+        no_duplicated.append(list_value)
